e7b7649e834cecf73e68df5d5f671c11cc4e9769
vickspanda <116336028+vickspanda@users.noreply.github.com>
2024-06-28 01:53:35 +0530

    Delete ad_logout.php

--- ad_logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    echo "<html><head><title>Logging Out</title></head><html>";
-    session_start();
-    session_destroy();
-    session_abort();
-    echo "<script>window.alert('Logged Out Successfully !!!');window.location.href='admin_login.html'</script>"
-?>
